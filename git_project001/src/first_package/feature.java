a7799ebb0681ca473e571eb9ad8bf09a30719b30
Mehmet <mlalek@gmail.com>
2020-08-08 20:13:57 +0200

    last update

---
+++ git_project001/src/first_package/feature.java
@@ -0,0 +1,10 @@
+package first_package;
+
+public class feature {
+
+	public static void main(String[] args) {
+		System.out.println("This is my commit from feature branch");
+
+	}
+
+}
---
+++ git_project001/src/first_package/firstClass.java
@@ -0,0 +1,12 @@
+package first_package;
+
+
+
+public class firstClass {
+
+	public static void main(String[] args) {
+		 System.out.println("Our first commit");
+		 
+	}
+
+}
