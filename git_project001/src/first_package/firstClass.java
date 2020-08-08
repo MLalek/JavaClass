8a334233a39d148089053bbb4d520c980d0e3e02
Mehmet <mlalek@gmail.com>
2020-08-08 20:33:53 +0200

    commit third in master

--- git_project001/src/first_package/firstClass.java
+++ git_project001/src/first_package/firstClass.java
@@ -6,7 +6,8 @@ public class firstClass {
 
 	public static void main(String[] args) {
 		 System.out.println("Our first commit");
-		 
+		 System.out.println("Our second commit");
+		 System.out.println("Our third commit");
 	}
 
 }
